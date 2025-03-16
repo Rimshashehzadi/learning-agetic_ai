34315fabcafb3e6c5e01a49884b8724bf3a2c9ec
rimsha_bibi <108316811+Rimshashehzadi@users.noreply.github.com>
2025-03-16 15:44:30 +0500

    litellm

--- crewai/hello_agent/src/hello_agent/main1.py
+++ crewai/hello_agent/src/hello_agent/main1.py
@@ -4,16 +4,23 @@ from litellm import completion
 
 
 class LitellmFlow(Flow):
+    @start()
     def start_function(self):
         output = completion(
+            model="gemini/gemini-2.0-flash",
             messages=[
                 {
                     "role":"user",
-                    "content":"Who is the president of the United States?"
+                    "content":"Who is the President of United State?"
                 }
             ]
         )
+        final=output["choices"][0]["message"]["content"]
+        print(final)
        
        
 
-    def
+def run_flow():
+    obj = LitellmFlow()
+    obj.kickoff()
+    
