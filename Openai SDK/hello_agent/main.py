0f5520e50af49360e0d8b7764b5774cae6446e8b
rimsha_bibi <108316811+Rimshashehzadi@users.noreply.github.com>
2025-04-22 23:30:44 +0500

    openai_SDK

--- Openai SDK/hello_agent/main.py
+++ Openai SDK/hello_agent/main.py
@@ -1,6 +1,49 @@
-def main():
-    print("Hello from hello-agent!")
+import os
+from dotenv import load_dotenv # type: ignore
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel # type: ignore
+from agents.run import RunConfig # type: ignore
+import asyncio
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+# Check if the API key is present; if not, raise an error
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="google/gemini-flash-1.5-8b",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-1.5-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+
+async def main():
+    agent = Agent(
+        name="Assistant",
+        instructions="You are helpful Assistent.",
+        model=model
+    )
+
+    result = await Runner.run(agent, "Tell me about recursion in programming.", run_config=config)
+    print(result.final_output)
+    # Function calls itself,
+    # Looping in smaller pieces,
+    # Endless by design.
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
